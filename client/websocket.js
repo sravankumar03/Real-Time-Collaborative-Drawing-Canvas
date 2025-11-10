ed699ecf4a6b0e388f97e4eb5f647b42291fdfbf
sravankumar03 <110688196+sravankumar03@users.noreply.github.com>
2025-11-10 22:12:02 +0530

    Update WebSocket connection URL and options

--- client/websocket.js
+++ client/websocket.js
@@ -1,10 +1,12 @@
 // client/websocket.js
-const socket = io({
+const socket = io("https://real-time-collaborative-drawing-canvas-t8dg.onrender.com", {
+  transports: ["websocket"],
   reconnection: true,
   reconnectionDelay: 1000,
   reconnectionDelayMax: 5000,
-  reconnectionAttempts: 5
+  reconnectionAttempts: 5,
 });
+
 
 function joinRoom(roomId = 'main', name = 'Anon') {
   if (!socket.connected) {
